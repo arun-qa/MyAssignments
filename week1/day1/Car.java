5eab2435ba10103cc15e397dd442475e748d474d
arun-qa <69797268+arun-qa@users.noreply.github.com>
2022-10-01 21:39:21 +0530

    Add files via upload
    week 1

---
+++ week1/day1/Car.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Car {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 1234;
+		byte wheels = 4;
+		short gear = 6;
+		long kilometer = 300000;
+		float speed = 50.55F;
+		boolean isDiesel = false;
+		char capacity = '4';
+		String colour = "red";
+		
+		
+		
+
+	}
+
+}
---
+++ week1/day1/Factorial.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=5;
+		int c=1;
+		{
+			for(int i=1;i<=n;i++)
+			c=c*i;
+		}
+		System.out.println(c);
+
+	}
+
+}
