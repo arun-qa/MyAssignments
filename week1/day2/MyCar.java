e30cbd1e30d20a63c25c3d8c3b52fb8788ea7f44
Arun G <69797268+arun-qa@users.noreply.github.com>
2022-10-05 07:52:36 +0530

    Add files via upload
    week1day2

---
+++ week1/day2/MyCar.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car obj = new Car();
+		obj.printCarName();
+		int num = obj.getRegNumber();
+		System.out.println(num);
+		String colour = obj.getColour();
+		System.out.println(colour);
+		boolean puncture = obj.isCarPunctured();
+		System.out.println(puncture);
+		int sum = obj.addThreeNumbers(5, 10, 15);
+		System.out.println(sum);
+		//int sub = obj.subTwoNumbers(10, 5);
+		//System.out.println(sub);
+		float mul = obj.multiplyThreeNumbers(2.1F, 3.1F, 4.1F);
+		System.out.println(mul);
+		int div = obj.divTwoNumbers(10, 2);
+		System.out.println(div);
+
+	}
+
+}
